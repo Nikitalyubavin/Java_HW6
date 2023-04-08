e9aec2b1d30458c20ad342a1bbb13beeb90aaeb1
Nikita <mail@gmail.com>
2023-04-09 02:37:46 +0300

    First commit

--- Notebooks.java
+++ Notebooks.java
@@ -15,9 +15,58 @@
 /**
  * Notebooks
  */
-public class Notebooks {
 
-    public static void main(String[] args) {
+import java.util.*;
+
+public class Notebooks {
+    static void ComputerSearch() {
+        SearchMethod("Введите модель: ");
         
+    }
+
+    static void SearchMethod(String mess) {
+        System.out.print(mess);
+        Scanner sc = new Scanner(System.in);
+        String model = sc.nextLine();
+        sc.close();
+        System.out.println(model);
+    }
+    
+    public static void main(String[] args) {
+        Map<Integer, Computers> comp = new HashMap<>();
+        comp.put(1, new Computers("Xiaomi", "RedmiBook 15",
+                8, 3, 256, "Windows", "Intel UHD Graphics"));
+        
+        
+        //for (Map.Entry<Integer, Computers> entry : comp.entrySet()) {
+        //    System.out.println(entry.getValue().toString());   
+        //}
+        ComputerSearch();
+    }
+}
+
+class Computers {
+    private String computerModel;
+    private String computerSeries;
+    private Integer computerRam;
+    private Integer computerCpu;
+    private Integer computerSsd;
+    private String computerOs;
+    private String computerVideo;
+
+    public Computers(String model, String series, Integer ram, Integer cpu, Integer ssd, String os, String videocard){
+        this.computerModel = model;
+        this.computerSeries = series;
+        this.computerRam = ram;
+        this.computerCpu = cpu;
+        this.computerSsd = ssd;
+        this.computerOs = os;
+        this.computerVideo = videocard;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("model: %s, series: %s, RAM: %d, CPU rate: %d, SSD: %d, system: %s, graphics: %s",
+                            computerModel, computerSeries, computerRam, computerCpu, computerSsd, computerOs, computerVideo);
     }
 }
